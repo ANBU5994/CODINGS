e44356d24fc91d5cc80dacec55f6b5fee5cdd17d
Anbazhagan R <147583133+ANBU5994@users.noreply.github.com>
2025-01-26 18:32:38 +0530

    Hacker rank problem.py

--- Hacker rank problem.py
+++ Hacker rank problem.py
@@ -1,19 +1,8 @@
-
 #Given an integer, , perform the following conditional actions:
-
 #If  is odd, print Weird
 #If  is even and in the inclusive range of2  to5 , print Not Weird
 #If  is even and in the inclusive range of 6 to 20 , print Weird
 #If  is even and greater than 20 , print Not Weird 
-
-import math
-import os
-import random
-import re
-import sys
-
-
-
 if __name__ == '__main__':
     n = int(input().strip())
     if n%2  !=0 :
