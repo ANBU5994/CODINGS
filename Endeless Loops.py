af8fb551283bd5b5933eac8a748947753f61031e
Anbazhagan R <147583133+ANBU5994@users.noreply.github.com>
2025-01-30 21:28:38 +0530

    Add files via upload

---
+++ Endeless Loops.py
@@ -0,0 +1,9 @@
+'''Imagine you‟re on a quest to discover never-ending loops. Create a program that 
+demonstrates endless journeys:  How will you set up these loops to keep running forever, showcasing their 
+infinite nature?'''
+
+print("Infinite Looping")
+
+while (True):  
+    print(" While Loop Starts")
+
