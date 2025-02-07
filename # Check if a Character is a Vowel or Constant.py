5c3e4d94199e46dd2db542214a45e1b01e788b57
Anbazhagan R <147583133+ANBU5994@users.noreply.github.com>
2025-02-07 20:42:44 +0530

    Add files via upload

---
+++ # Check if a Character is a Vowel or Constant.py
@@ -0,0 +1,9 @@
+# Check if a Character is a Vowel or Consonant:
+# Given a character, determine if it is a vowel (a, e, i, o, u) or a consonant using conditional statements.
+
+n=input("enter the string ")
+for i in n:
+    if i in ("a","e","i","o","u"):
+        print("vowels")
+    else:
+        print("constant")
